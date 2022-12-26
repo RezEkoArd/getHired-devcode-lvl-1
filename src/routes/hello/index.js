2f9d8cadcd8160b5ebf0e09fcb4b5b7828cc2cbf
Rezekoard <rezkyardianto1222@gmail.com>
2022-12-26 08:10:35 +0700

    first upload

---
+++ src/routes/hello/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { HelloWorld } from "../../components/HelloWorld";
+import "../../assets/styles/style.css";
+
+function index() {
+  return (
+   <>
+    <HelloWorld/>
+   </>
+  )
+}
+
+export default index
